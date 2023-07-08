15bf637061fddd9bb48665cecd4df32fece8599e
SurajChaudhari80 <90961197+SurajChaudhari80@users.noreply.github.com>
2023-07-08 19:20:50 +0530

    Add files via upload

---
+++ Quiz-Application/src/quiz/application/Rules.java
@@ -0,0 +1,72 @@
+package quiz.application;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class Rules extends JFrame implements ActionListener{
+
+    String name;
+    JButton start, back;
+    
+    Rules(String name) {
+        this.name = name;
+        getContentPane().setBackground(Color.WHITE);
+        setLayout(null);
+        
+        JLabel heading = new JLabel("Welcome " + name + " to Quiz game!");
+        heading.setBounds(50, 20, 700, 30);
+        heading.setFont(new Font("Times New Roman", Font.BOLD, 28));
+        heading.setForeground(new Color(30, 144, 254));
+        add(heading);
+        
+        JLabel rules = new JLabel();
+        rules.setBounds(20, 90, 700, 350);
+        rules.setFont(new Font("Tahoma", Font.PLAIN, 16));
+        rules.setText(
+            "<html>"+ 
+                "1. There are ten basic questions of Core Java." + "<br><br>" +
+                "2. Each question of ten marks." + "<br><br>" +
+                "3. Time to solve each question is 15 seconds." + "<br><br>" +
+                "4. 50-50 Lifeline is present which remove two wrong answers." + "<br><br>" +
+                "5. Only one times we can use lifeline." + "<br><br>" +
+                "6. We can submit the quiz only at the end."+ "<br><br>" +
+                "7. The final score will be shown after sumitting quiz." + "<br><br>" +
+                "8. All the Best!!!" + "<br><br>" +
+            "<html>"
+        );
+        add(rules);
+        
+        back = new JButton("Back");
+        back.setBounds(250, 500, 100, 30);
+        back.setBackground(new Color(30, 144, 254));
+        back.setForeground(Color.WHITE);
+        back.addActionListener(this);
+        add(back);
+        
+        start = new JButton("Start");
+        start.setBounds(400, 500, 100, 30);
+        start.setBackground(new Color(30, 144, 254));
+        start.setForeground(Color.WHITE);
+        start.addActionListener(this);
+        add(start);
+        
+        setSize(800, 650);
+        setLocation(350, 100);
+        setVisible(true);
+    }
+    
+    public void actionPerformed(ActionEvent ae) {
+        if (ae.getSource() == start) {
+            setVisible(false);
+            new Quiz(name);
+        } else {
+            setVisible(false);
+            new Login();
+        }
+    }
+    
+    public static void main(String[] args) {
+        new Rules("User");
+    }
+}
